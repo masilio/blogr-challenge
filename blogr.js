e107cae6e14b4da9cc8274ebe6bf078a4c04b176
masilio <felinshimiyimana@gmail.com>
2021-06-19 20:50:01 +0200

    fourth commit

--- blogr.js
+++ blogr.js
@@ -20,38 +20,3 @@ function toggleMenu() {
 
 /* Event Listener */
 toggle.addEventListener("click", toggleMenu, false);
-
-
-const items = document.querySelectorAll(".item");
-
-/* Activate Submenu */
-function toggleItem() {
-    if (this.classList.contains("submenu-active")) {
-        this.classList.remove("submenu-active");
-    } else if (menu.querySelector(".submenu-active")) {
-        menu.querySelector(".submenu-active").classList.remove("submenu-active");
-        this.classList.add("submenu-active");
-    } else {
-        this.classList.add("submenu-active");
-    }
-}
-
-/* Event Listeners */
-for (let item of items) {
-    if (item.querySelector(".submenu")) {
-        item.addEventListener("click", toggleItem, false);
-        item.addEventListener("keypress", toggleItem, false);
-    }
-}
-
-/* Close Submenu From Anywhere */
-function closeSubmenu(e) {
-    let isClickInside = menu.contains(e.target);
-
-    if (!isClickInside && menu.querySelector(".submenu-active")) {
-        menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    }
-}
-
-/* Event listener */
-document.addEventListener("click", closeSubmenu, false);
